f0be68be2373bc6c559d6e2fc0811634ccb219a1
Nikhitha Chowdary <nikhithachowdary@Nikhithas-MacBook-Pro.local>
2024-12-05 12:50:56 -0800

    create API endpoint to delete item from shopping cart

--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,11 +1,12 @@
 import express from "express";
 import passport from "../config/passport.js";
-import { getCart, addToCart, syncCart } from "../controllers/cartController.js";
+import { getCart, addToCart, syncCart, deleteItem } from "../controllers/cartController.js";
 
 const router = express.Router();
 
 router.get("/", passport.authenticate('jwt', {session : false}), getCart);
 router.post("/", passport.authenticate('jwt', {session : false}), addToCart);
 router.post("/sync", passport.authenticate('jwt', {session : false}), syncCart);
+router.delete("/:id", passport.authenticate('jwt', {session : false}), deleteItem);
 
 export default router;
