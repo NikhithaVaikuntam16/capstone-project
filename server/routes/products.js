0dc566e1b6ecbaf0de9a5370011c2800a66e1bf7
Nikhitha Chowdary <nikhithachowdary@Nikhithas-MacBook-Pro.local>
2024-12-06 11:50:34 -0800

    update stock availability in products table

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { getProductDetails, getProducts, getStockAvailability } from "../controllers/productsController.js";
+import { getProductDetails, getProducts, getStockAvailability, updateStock } from "../controllers/productsController.js";
 
 const router = express.Router();
 
 router.get("/", getProducts);
 router.get("/:id", getProductDetails);
 router.get("/:id/stock", getStockAvailability);
+router.put("/update-stock", updateStock);
 
 export default router;
