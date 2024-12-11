7cd7cb2e6b74984ca611444a5b9eb552cd4e23ca
Nikhitha Chowdary <nikhithachowdary@Nikhithas-MacBook-Pro.local>
2024-12-11 11:52:36 -0800

    view orders on orders page

--- server/controllers/ordersController.js
+++ server/controllers/ordersController.js
@@ -1,5 +1,5 @@
 import pool from "../config/db.js";
-import { insertOrderDetails, insertAllOrderedItems } from "../models/ordersModel.js";
+import { insertOrderDetails, insertAllOrderedItems, getOrdersByUserId, getTotalItemsByOrderId } from "../models/ordersModel.js";
 import { updateStockById } from "../models/productsModel.js";
 
 export const insertOrder = async (req, res) => {
@@ -20,7 +20,7 @@ export const insertOrder = async (req, res) => {
             await updateStockById(client, product.quantity, product.id);
             await insertAllOrderedItems(client, orderId, product.id, product.quantity, product.price);
         }
-        
+
         await client.query("COMMIT");
         return res.status(200).json({ success: true, orderId });
     }catch(err) {
@@ -28,5 +28,24 @@ export const insertOrder = async (req, res) => {
         return res.status(500).json({error: "Something went wrong while inserting an order"});
     }finally {
         client.release();
+    }
+}
+
+export const getOrders = async (req, res) => {
+    const userId = req.user.id;
+    try {
+        const orders = await getOrdersByUserId(userId);
+        if(orders.length > 0) {
+            const ordersArray = [];
+            for (const order of orders) {
+                const totalItems = await getTotalItemsByOrderId(order.id);
+                ordersArray.push({...order, totalItems});
+            }
+            return res.status(200).json({orders: ordersArray});
+        }else {
+            return res.status(404).json({orders:[], message: "No orders found" });
+        }
+    }catch(err) {
+        return res.status(500).json({error: "Something went wrong while fetching orders"});
     }
 }
--- server/routes/orders.js
+++ server/routes/orders.js
@@ -1,9 +1,10 @@
 import express from "express";
 import passport from "../config/passport.js";
-import { insertOrder } from "../controllers/ordersController.js";
+import { insertOrder, getOrders } from "../controllers/ordersController.js";
 
 const router = express.Router();
 
 router.post("/", passport.authenticate('jwt', {session : false}), insertOrder);
+router.get("/", passport.authenticate('jwt', {session : false}), getOrders);
 
 export default router;
